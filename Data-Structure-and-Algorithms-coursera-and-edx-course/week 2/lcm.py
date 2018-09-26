d7ead10216adc27fb7595b41d9fc01210fc7ea72
ezzmuhamed <43146028+ezzmuhamed@users.noreply.github.com>
2018-09-26 06:00:51 +0200

    Update lcm.py

--- Data-Structure-and-Algorithms-coursera-and-edx-course/week 2/lcm.py
+++ Data-Structure-and-Algorithms-coursera-and-edx-course/week 2/lcm.py
@@ -1,1 +1,18 @@
 
+# Uses python2
+
+def gcd(a, b):
+    """Return greatest common divisor using Euclid's Algorithm."""
+    while b:      
+        a, b = b, a % b
+    return a
+
+def lcm(a, b):
+    """Return lowest common multiple."""
+    return a * b // gcd(a, b)
+
+
+a,b = raw_input().split()
+a = int(a)
+b = int(b)
+print(lcm(a, b))
