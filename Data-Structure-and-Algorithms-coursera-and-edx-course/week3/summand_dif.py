c2607e9abab8aadf63b043586796ed3875163df4
ezzmuhamed <43146028+ezzmuhamed@users.noreply.github.com>
2018-09-26 06:13:19 +0200

    Update summand_dif.py

--- Data-Structure-and-Algorithms-coursera-and-edx-course/week3/summand_dif.py
+++ Data-Structure-and-Algorithms-coursera-and-edx-course/week3/summand_dif.py
@@ -1,1 +1,26 @@
+#uses python2
 
+def optimal_summands(n): 
+    summands = [1]
+    n -= 1
+    
+    while n:
+        last_element = summands[-1] 
+        if (last_element + 1) * 2 <= n:
+            n -= last_element + 1
+            summands.append(last_element + 1)
+        else:
+            if last_element >= n:
+                n += summands.pop()
+            summands.append(n)
+            n = 0
+
+    return summands
+
+
+n = int(input())
+summands = optimal_summands(n)
+print(len(summands))
+for x in summands:
+    print x,
+        
