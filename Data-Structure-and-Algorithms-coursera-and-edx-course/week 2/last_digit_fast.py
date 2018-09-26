095ef2cca88ed36ff5f96d368dea316e552406d4
ezzmuhamed <43146028+ezzmuhamed@users.noreply.github.com>
2018-09-26 05:57:46 +0200

    Update last_digit_fast.py

--- Data-Structure-and-Algorithms-coursera-and-edx-course/week 2/last_digit_fast.py
+++ Data-Structure-and-Algorithms-coursera-and-edx-course/week 2/last_digit_fast.py
@@ -1,1 +1,10 @@
+def last_digit_fast(n):
+    if n<=1 :
+        return n
+    a,b = 1,1
+    for i in range(n-1):
+        a,b = b,(a+b)%10
+    return(a)
 
+n = int(input())
+print(last_digit_fast(n))
