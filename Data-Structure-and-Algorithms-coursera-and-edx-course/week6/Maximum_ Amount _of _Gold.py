c54e14a1579d6acf2a2dfef0d06926559c84335c
ezzmuhamed <43146028+ezzmuhamed@users.noreply.github.com>
2018-09-26 07:42:55 +0200

    Update Maximum_ Amount _of _Gold.py

--- Data-Structure-and-Algorithms-coursera-and-edx-course/week6/Maximum_ Amount _of _Gold.py
+++ Data-Structure-and-Algorithms-coursera-and-edx-course/week6/Maximum_ Amount _of _Gold.py
@@ -1,1 +1,22 @@
 
+#uses python2
+
+def knapSack(W, wt, n): 
+    K = [[0 for x in range(W+1)] for x in range(n+1)] 
+  
+    # Build table K[][] in bottom up manner 
+    for i in range(n+1): 
+        for w in range(W+1): 
+            if i==0 or w==0: 
+                K[i][w] = 0
+            elif wt[i-1] <= w: 
+                K[i][w] = max(wt[i-1]+K[i-1][w-wt[i-1]],  K[i-1][w]) 
+            else: 
+                K[i][w] = K[i-1][w] 
+  
+    return K[n][w]
+
+W,n = map(int,raw_input().split())
+wt = raw_input()
+wt = [int(x) for x in wt.split()]
+print knapSack(W , wt, n) 
