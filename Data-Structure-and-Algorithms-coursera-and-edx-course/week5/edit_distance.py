310a62072ada4e29423a52e457ec0d9ce2e5a285
ezzmuhamed <43146028+ezzmuhamed@users.noreply.github.com>
2018-09-26 06:29:49 +0200

    Update edit_distance.py

--- Data-Structure-and-Algorithms-coursera-and-edx-course/week5/edit_distance.py
+++ Data-Structure-and-Algorithms-coursera-and-edx-course/week5/edit_distance.py
@@ -1,1 +1,23 @@
 
+#uses python2
+
+def edit_distance(s, t):
+	
+	m , n = len(s) , len(t)
+	dp=[[0 for i in range(n+1)] for j in range(m+1)]
+	#print dp
+	for i in range(m+1): dp[i][0]=i
+	#print dp
+	for j in range(n+1): dp[0][j]=j;
+	#print dp
+	for i in range(1,m+1):
+		for j in range(1,n+1):
+                        #print min(dp[i-1][j-1] + (0 if s[i-1]==t[j-1] else 1)
+		 	dp[i][j]=min(dp[i-1][j-1] + (0 if s[i-1]==t[j-1] else 1) , 
+						min(dp[i-1][j]+1,dp[i][j-1]+1) )
+	return dp[m][n]
+
+
+s = raw_input()
+t = raw_input()
+print (edit_distance(s, t))
