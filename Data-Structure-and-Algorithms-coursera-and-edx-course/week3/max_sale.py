33d5af1f57c376b5de22991d991c93fcb3b3187e
ezzmuhamed <43146028+ezzmuhamed@users.noreply.github.com>
2018-09-26 06:14:42 +0200

    Update max_sale.py

--- Data-Structure-and-Algorithms-coursera-and-edx-course/week3/max_sale.py
+++ Data-Structure-and-Algorithms-coursera-and-edx-course/week3/max_sale.py
@@ -1,1 +1,43 @@
 
+#uses python2
+
+def largest_number(a):
+    
+    nums = map(str, a)
+    lst = []
+    
+     
+    
+    for i in range (len(a)):
+        if a[i]>10:
+            lst.append(a[i] / 10)
+            lst.append(a[i]%10)
+        elif a[i] == 10:
+            lst.append(a[i] / 10)
+            lst.append(0)
+        else:
+            lst.append(a[i])
+            
+    if (len(lst)) == (len(a)*2):
+        a.sort(reverse=True)
+        return a
+    else:
+        lst.sort(reverse=True)
+        
+        return lst
+    
+    
+        
+
+   
+
+
+a = int(input())
+data = raw_input()
+data =[int (x) for x in data.split()]
+a = data[0:]
+largest = largest_number(a)
+txt = ''
+for x in largest:
+    txt += str(x)
+print txt    
